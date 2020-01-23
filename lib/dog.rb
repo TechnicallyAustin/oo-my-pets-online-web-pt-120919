c99facdf139cb03f7ed7253c8f2824c302a5edc5
Austin Johnson <technicallyaustin@gmail.com>
2020-01-23 03:02:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,17 @@
 class Dog
   @@all = []
-  attr_accessor :name, :breed 
-  attr_reader:owner
+  attr_accessor :bree, :owner, :mood 
+  attr_reader:name
   def initialize(name)
     @name = name 
     @owner = owner
+    @mood = mood 
     @@all << self 
   end
+  
+  def self.all
+    @@all
+  end
+  
   # code goes here
 end

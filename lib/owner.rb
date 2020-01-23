7cc1a4859ebe658a3ca2b2229f5e7d1a0beefb88
Austin Johnson <technicallyaustin@gmail.com>
2020-01-23 03:05:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,7 +3,7 @@ class Owner
   @@all = []
   attr_accessor :pets
   attr_reader :species, :name
-  def initialize(name)
+  def initialize(name, species = "human")
     @name = name 
     @species = species 
     @@all << self 
